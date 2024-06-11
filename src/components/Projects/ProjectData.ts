63ecf8410517bbba6e6418ef9016b440f80b2b77
mathiasgugu <matgum51873@stud.noroff.no>
2024-06-11 13:55:38 +0200

    thing

---
+++ src/components/Projects/ProjectData.ts
@@ -0,0 +1,33 @@
+interface project {
+  title: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  link: string;
+}
+
+export default [
+  {
+    title: "Receipt Scanner - Receipto",
+    description:
+      "Receipto is a receipt scanner app that allows users to scan their receipts and store them in a digital format.",
+    image: "/blog-placeholder-1.jpg",
+    imageAlt: "Project 1",
+    link: "https://www.google.com",
+  },
+  {
+    title: "Holidaze - Booking App",
+    description:
+      "Holidaze is a booking app that allows users to book hotels and accommodations for their holidays.",
+    image: "/blog-placeholder-2.jpg",
+    imageAlt: "Project 1",
+    link: "https://www.google.com",
+  },
+  {
+    title: "FinnJobb - Job Search App",
+    description: "This is project 1",
+    image: "/blog-placeholder-3.jpg",
+    imageAlt: "Project 1",
+    link: "https://www.google.com",
+  },
+];
