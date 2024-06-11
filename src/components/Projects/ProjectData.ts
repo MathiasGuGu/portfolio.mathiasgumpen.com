bbd582638dd971053e02b57db47b6a5dcfeba97b
mathiasgugu <matgum51873@stud.noroff.no>
2024-06-11 20:58:27 +0200

    something

--- src/components/Projects/ProjectData.ts
+++ src/components/Projects/ProjectData.ts
@@ -10,7 +10,7 @@ export default [
   {
     title: "Receipt Scanner - Receipto",
     description:
-      "Receipto is a receipt scanner app that allows users to scan their receipts and store them in a digital format.",
+      "Receipto is a finance app that allows users to scan receipts and categorize their spending into custom categories and tags. Receipto allows users to track their spending and see where their money is going.",
     image: "/blog-placeholder-1.jpg",
     imageAlt: "Project 1",
     link: "https://www.google.com",
@@ -25,7 +25,8 @@ export default [
   },
   {
     title: "FinnJobb - Job Search App",
-    description: "This is project 1",
+    description:
+      "FinnJobb is a job search app that scrapes work data from Finn.no and makes it easier and faster for a user to unserstand what criteria they must meet to get a job.",
     image: "/blog-placeholder-3.jpg",
     imageAlt: "Project 1",
     link: "https://www.google.com",
